ac26aa650fc0f7f92124a0db4b4a7c813c92c152
Ugo Pozo <ugopozo@camara.sp.gov.br>
2018-01-17 16:02:51 -0200

    Year change.

--- anubis/views/app_view_mixin.py
+++ anubis/views/app_view_mixin.py
@@ -308,7 +308,7 @@ class AppViewMixin(StateViewMixin):
 
         return {
             "title": "Anubis Search Interface",
-            "footer": ("© 2016-17, Câmara Municipal de São Paulo, "
+            "footer": ("© 2016-18, Câmara Municipal de São Paulo, "
                        "Secretaria de Documentação, "
                        "Equipe de Documentação do Legislativo."),
             "baseURL": self.base_url,
